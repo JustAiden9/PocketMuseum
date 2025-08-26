1aa096734232fb9a43f4cce8eb5855f834f6821a
Aiden <aidenbak9@gmail.com>
2025-08-26 09:55:21 -0500

    Initial Commit

---
+++ Pocket Museum/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  Pocket Museum
+//
+//  Created by Aiden Baker on 8/26/25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ Pocket MuseumTests/Pocket_MuseumTests.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_MuseumTests.swift
+//  Pocket MuseumTests
+//
+//  Created by Aiden Baker on 8/26/25.
+//
+
+import Testing
+@testable import Pocket_Museum
+
+struct Pocket_MuseumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
